70959e87d10d1a8e08afecf04bdd201d0498da79
BrMacCath <brian.collery@colostate.edu>
2023-11-02 09:40:54 -0600

    Update style. Have the button without functionality yet.

--- djangoSite/settings.py
+++ djangoSite/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'djangoSite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['testWeb/djangoSite/templates','testWeb/WebsiteProgress/templates/WebsiteProgress','testWeb/Math340/templates/Math340'],
+        'DIRS': ['djangoSite/templates','WebsiteProgress/templates/WebsiteProgress','Math340/templates/Math340'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
