3a812b1e3197a385ad6365ca8f5e3e6f7e006c44
BrMacCath <brian.collery@colostate.edu>
2023-11-22 00:12:10 -0700

    Fixed a lot of CSS. Got topics for linear algebra. However javascript is still not working.

--- Lin/admin.py
+++ Lin/admin.py
@@ -5,6 +5,6 @@ from .models import Topic
 # Register your models here.
 class TopicAdmin(admin.ModelAdmin):
     fieldsets =[ ("Properties",{"fields": ["weight","topic_location","topic_description","topic_webpage", "topic_title"]}  ), ]
-    list_display = ["weight","topic_location"]
+    list_display = ["topic_title","topic_description"]
 
 admin.site.register(Topic,TopicAdmin)
