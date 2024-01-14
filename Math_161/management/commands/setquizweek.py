48bf906bb7ba19e8ae2142583ea1ac18ef04bed7
BrMacCath <brian.collery@colostate.edu>
2024-01-14 14:19:36 -0700

    Is this uploading all the data to the internet?

--- Math_161/management/commands/setquizweek.py
+++ Math_161/management/commands/setquizweek.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     help = "Notices which weeks have a quiz connected to them and sets quiz_boolean to true. Set's all other to false."
     
     def handle(self, *args: Any, **options: Any) -> str | None:
-        releaseDate = datetime.datetime(2024,1,20,9,tzinfo=pytz.UTC)
+        releaseDate = datetime.datetime(2024,1,21,9,tzinfo=pytz.UTC)
         for week in Week.objects.all():
             week.quiz_Boolean = False
         for week in Week.objects.all():
